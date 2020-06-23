37668df52afa454f9578a3813a303d39c0715244
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-06-23 18:33:18 -0300

    Create main entry point

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <Router>
+    <GlobalStyle />
+    <ToastContainer />
+    <Routes />
+  </Router>
+);
+
+export default App;
