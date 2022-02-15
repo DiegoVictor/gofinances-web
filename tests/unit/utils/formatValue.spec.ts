7058ed3d7cf999179ace04dbebba3a3a12e65aa4
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-15 19:19:10 -0300

    Move to unit test folder

--- tests/unit/utils/formatValue.spec.ts
+++ tests/unit/utils/formatValue.spec.ts
@@ -1,4 +1,4 @@
-import formatValue from '../../src/utils/formatValue';
+import formatValue from '../../../src/utils/formatValue';
 
 describe('formatValue', () => {
   it('should be able to format a value', async () => {
