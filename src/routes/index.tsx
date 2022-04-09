1bc326e43dbe9964b0844373d0fa38bcfa2c65f4
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-09 11:22:47 -0300

    Fix types

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Import from '../pages/Import';
 
-export default () => (
+export default (): JSX.Element => (
   <Routes>
     <Route path="/" element={<Dashboard />} />
     <Route path="/import" element={<Import />} />
